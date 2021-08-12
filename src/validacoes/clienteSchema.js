0e9660cac622f04624a159ef378ccd288af539d8
Vanessa  Carvalho <80365265+DevVane@users.noreply.github.com>
2021-08-12 15:36:44 -0300

    Ajustando validação do telefone do cliente

--- src/validacoes/clienteSchema.js
+++ src/validacoes/clienteSchema.js
@@ -1,9 +1,11 @@
 const yup = require('./yup');
+const phone = require('yup-phone');
+
 
 const clienteSquema = yup.object().shape({
     nome: yup.string().max(100).trim().required(),
     email: yup.string().max(100).email().required(),
-    telefone: yup.string().max(25).trim(),
+    telefone: yup.string().max(25).trim().phone("BR", true, '${path} deve ser um número de telefone válido para região BR').required(),
     senha: yup.string().trim().required()
  
 })
